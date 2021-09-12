bdccb3a0699529f38ceda248398891618841a7cb
Pranit Yadav <42487480+Pranit46@users.noreply.github.com>
2021-09-12 10:24:31 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,37 @@
+function  myFunction(){
+    let text = document.getElementById("content").value
+    console.log(text.length)
+    
+    document.getElementById("count").innerHTML = text.length
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
